f7a66d21e3e7bb22b2a4312ead76d6fa181a672d
Leandro <officialdevmi@gmail.com>
2021-09-17 04:32:10 -0700

    Add homepage styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import './Nav.css';
 
 const Nav = (props) => {
+  const location = useLocation();
+  const { name } = location.state || 'World&apos;s CoronaVirus Statistics';
   const { display } = props;
   return (
     <>
       <div className="Navigation">
-        <span className={display}><Link to="/" className="Link">&lt; Back</Link></span>
+        <span className={display}>
+          <Link to="/" className="Link">
+            &lt; Back
+          </Link>
+        </span>
         <span className="Logo">CoronaWide</span>
       </div>
       <div className="Slogan-Cont">
         <img className="Logo" src={logo} alt="covid logo" />
-        <span className="Slogan">World&apos;s CoronaVirus Statistics</span>
+        {name && (
+          <span className="Slogan">{name}</span>
+        )}
+        {!name && (
+          <span className="Slogan">World&apos;s CoronaVirus Statistics</span>
+        )}
       </div>
     </>
   );
